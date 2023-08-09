2346841f7880084254c343f212123b9560b7f8b4
LUCAS FAGUNDES <lucasfagundes@univille.br>
2023-08-09 19:58:29 -0300

    ajuste das pastas

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,3 @@
-package main;
-
 public class Calculadora {
 
     public double somar(double valor1, double valor2) {
--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -1,6 +1,3 @@
-package test.java;
-
-import main.Calculadora;
 import org.junit.Assert;
 import org.junit.Test;
 
